63d8e77b4999db774a9dfdb680cba422d2e2adcd
Markus Hubig <mhubig@hey.com>
2022-07-01 14:19:49 +0200

    Strip it Down to the bare minimum ..

--- build.gradle.kt
+++ build.gradle.kt
@@ -1,5 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 buildscript {
     repositories {
         google()
@@ -8,9 +6,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:3.1.3")
         classpath(kotlin("gradle-plugin", version = "1.3.70"))
-        compileOnly("com.example.foo:tool-types:2022-05-10_55") // regex:^\\d+-\\d+-\\d+_(?<patch>\\d+)$
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle.kts files
+        compileOnly("com.example.foo:tool-types:2022-05-10_55")
     }
 }
 
